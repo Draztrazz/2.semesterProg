3b1cade8b698235d7202b3e4f4fc28aabf510b6a
Peter2960 <peterdreyer45@gmail.com>
2021-04-20 12:30:54 +0200

    added frontend signup

---
+++ Frontend_JS/Signup.js
@@ -0,0 +1,41 @@
+var form = document.getElementById('form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value
+    var email = document.getElementById('email').value
+    var age = document.getElementById('age').value
+    var firstname = document.getElementById('firstname').value
+    var lastname = document.getElementById('lastname').value
+    var gender = document.getElementById('gender').value
+    var bio = document.getElementById('bio').value
+    var admin = 0
+
+    fetch('http://localhost:7071/api/ApplikationsFunktion', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: username,
+            password: password,
+            email: email,
+            age: age,
+            firstname: firstname,
+            lastname: lastname,
+            gender: gender,
+            bio: bio,
+            admin: admin
+        }),
+        headers: {
+            "Content-Type": "application/json; charset-UTF-8"
+        }
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    }) .catch((err) =>{
+        console.log(err)
+    })
+})
