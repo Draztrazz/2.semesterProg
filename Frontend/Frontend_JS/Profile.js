f4f1ee7f3f9209706c48f6f550122b2d4e2789a5
OscarCBShait <oscarpedersen1999@hotmail.com>
2021-04-29 10:11:35 +0200

    making data commitments profile.js

--- Frontend/Frontend_JS/Profile.js
+++ Frontend/Frontend_JS/Profile.js
@@ -9,7 +9,15 @@ getButton.addEventListener("click", function(){
         .then((resp) => resp.json()
         )
         .then(function(data) {
-            console.log(data);
+            console.log(data)
+            document.getElementById("username").innerHTML = data[1].value
+            /*
+            document.getElementById("email").innerHTML = data[2].value
+            document.getElementById("firstname").innerHTML = data[3].value
+            document.getElementById("lastname").innerHTML = data[4].value
+            document.getElementById("dob").innerHTML = data[5].value
+            document.getElementById("gender").innerHTML = data[6].value
+            document.getElementById("bio").innerHTML = data[7].value*/
         })
         .catch(function(err){
             console.log(err)
