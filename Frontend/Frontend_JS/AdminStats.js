e2a354637832d5c98c4ad076a03e87ec1b964c45
OscarCBShait <oscarpedersen1999@hotmail.com>
2021-04-30 11:16:35 +0200

    changes to admin

---
+++ Frontend/Frontend_JS/AdminStats.js
@@ -0,0 +1,16 @@
+let jwt = localStorage.getItem("JWT");
+
+let showAllUsers = document.getElementById("showusers");
+
+updateButton.addEventListener('click', () => {
+    fetch(`http://localhost:7071/api/admin`)
+        .then((resp) => resp.json()
+        )
+        .then(function(data) {
+            console.log(data)
+            console.log("You have succesfully retrieved all of the great users")
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+})
