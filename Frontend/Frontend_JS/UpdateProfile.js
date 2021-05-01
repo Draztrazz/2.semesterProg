7c9adb42608012de36778c8238865f36793d8949
OscarCBShait <oscarpedersen1999@hotmail.com>
2021-05-01 08:59:07 +0200

    adding validation to admin and update profile func

--- Frontend/Frontend_JS/UpdateProfile.js
+++ Frontend/Frontend_JS/UpdateProfile.js
@@ -8,6 +8,79 @@ const lastname = document.getElementById('lastname');
 const gender = document.getElementById('gender');
 const bio = document.getElementById('bio');
 
+// check inputs
+function checkInputs(){
+    let inputValidated = true;
+
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const dobValue = dob.value;
+    const firstnameValue = firstname.value;
+    const lastnameValue = lastname.value;
+    
+    if (usernameValue == '' || usernameValue.length < 8 || usernameValue.length > 20) {
+        setErrorFor(username, 'Username must be between 8 and 20 characters long.');
+    } else {
+        setSuccesFor(username);
+    }
+    if (emailValue == '') {
+        setErrorFor(email, 'E-mail cannot be blank.');
+    } else {
+        setSuccesFor(email);
+    }
+    if (firstnameValue == '') {
+        setErrorFor(firstname, 'First name cannot be blank.');
+    } else if (firstnameValue.length > 20) {
+        setErrorFor(firstname, 'First name cannot be longer than 20 characters.');
+    } else {
+        setSuccesFor(firstname);
+    }
+    if (lastnameValue == '') {
+        setErrorFor(lastname, 'Last name cannot be blank.');
+    } else if (lastnameValue.length > 20) {
+        setErrorFor(lastname, 'Last name cannot be longer than 20 characters.');
+    } else {
+        setSuccesFor(lastname);
+    }
+    if (dobValue == '') {
+        setErrorFor(dob, 'Date of birth cannot be blank.');
+    } else if (calculate_age(dobValue) < 18) {
+        setErrorFor(dob, 'You must 18 or older to use this app.');
+    } else {
+        setSuccesFor(dob);
+    }
+    /*if (bioValue == ''){
+        bioValue = 'You have not yet written your own bio.'
+    }*/
+
+    if(inputValidated == true){
+        postUser();
+    } else {
+        return false
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+    small.innerText = message;
+
+    formControl.className = 'form-control error';
+
+    inputValidated = false;
+}
+
+function setSuccesFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control succes';
+}
+
+// calculate age
+function calculate_age(dob) {
+    var ageDif = Date.now() - new Date(dob).getTime();
+    var ageDate = new Date(ageDif);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
 
 window.addEventListener("load", function(){
 
